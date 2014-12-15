37a1ab03e34f00117f3e95a9beb1d10588d86d36
zeMirco <mirco.zeiss@gmail.com>
2014-12-15 18:50:08 +0100

    set tmp path and make path persitent

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -12,14 +12,14 @@ hg clone -u release https://code.google.com/p/go
 cd go/src
 ./all.bash
 
-# set golang path
+# set golang path for current shell and for reboot etc.
+export PATH=$PATH:/home/ubuntu/go/bin
 echo 'export PATH=$PATH:/home/ubuntu/go/bin' >> /home/ubuntu/.bashrc
 
 # make golang workspace
 cd /home/ubuntu
 mkdir gocode
+export GOPATH=/home/ubuntu/gocode
+export PATH=$PATH:$GOPATH/bin
 echo 'export GOPATH=$HOME/gocode' >> /home/ubuntu/.bashrc
 echo 'export PATH=$PATH:$GOPATH/bin' >> /home/ubuntu/.bashrc
-
-# activate changes
-source /home/ubuntu/.bashrc
