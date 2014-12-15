eb9cfbd9e920cc57651b257a2136351fa6ec768e
zeMirco <mirco.zeiss@gmail.com>
2014-12-15 17:55:56 +0100

    add home directory for ubuntu user

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -7,6 +7,7 @@ sudo apt-get upgrade -y
 sudo apt-get install mercurial git gcc libc6-dev -y
 
 # install golang
+cd /home/ubuntu
 hg clone -u release https://code.google.com/p/go
 cd go/src
 ./all.bash
